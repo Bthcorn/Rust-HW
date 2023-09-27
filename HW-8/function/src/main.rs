1cecb691e82d935ad9eb060798f9ba16fdd93478
Bthcorn <139036880+Bthcorn@users.noreply.github.com>
2023-09-27 10:35:24 +0700

    fix function

--- HW-8/function/src/main.rs
+++ HW-8/function/src/main.rs
@@ -1,16 +1,16 @@
 fn main() {
     let v = vec![
-        to_polar::Point {
+        function::Point {
             x: 1.0,
             y: 2.0,
         },
-        to_polar::Point {
+        function::Point {
             x: 3.0,
             y: 4.0,
         },
     ];
-    let v1 = to_polar::to_polar(&v);
-    let v2 = to_polar::to_cartesian(&v1);
+    let v1 = function::to_polar(&v);
+    let v2 = function::to_cartesian(&v1);
     println!("{:?}", v1);
     println!("{:?}", v2)
 }
